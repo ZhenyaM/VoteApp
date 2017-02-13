2c3b15b8aa4066cee2239fce39b8b18fe4293477
Evgeniy Muravitskiy <mypuk2009@gmail.com>
2017-02-13 18:15:43 +0200

    Add account entity

---
+++ src/main/java/com/vote/entity/Account.java
@@ -0,0 +1,13 @@
+package com.vote.entity;
+
+/**
+ * {@author Evgeniy}
+ */
+public interface Account {
+
+	String getEmail();
+
+	String getPassword();
+
+	String getRole();
+}
---
+++ src/main/java/com/vote/entity/AccountImpl.java
@@ -0,0 +1,37 @@
+package com.vote.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * {@author Evgeniy}
+ */
+@Entity
+@Table(name = "account")
+public class AccountImpl extends DomainIdObject implements Account {
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "role")
+	private String role;
+
+	@Override
+	public String getEmail() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getRole() {
+		return null;
+	}
+}
